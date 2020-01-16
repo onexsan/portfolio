5b271d5dacc92833337fca6285852a87a45d8238
Victoria Filileeva <onexsan@gmail.com>
2020-01-16 14:10:27 +0300

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,10 +20,10 @@ git add -A
 git commit -m 'deploy'
 
 ## if you are deploying to https://<USERNAME>.github.io
-#git push -f https://github.com/<USERNAME>/<REPO> master:gh-pages
+git push -f https://github.com/onexsan/filileeva-ls.git master:gh-pages
 
 ## if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://onexsan.github.io/filileeva-ls master:gh-pages 
+# git push -f https://onexsan.github.io/filileeva-ls master:gh-pages 
 
 cd -
 
