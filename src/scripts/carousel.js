7c0c0a4873f021e95ad9ee8822498559ae00fe8d
Victoria Filileeva <onexsan@gmail.com>
2020-01-25 18:27:44 +0300

    added a responsive review carousel

---
+++ src/scripts/carousel.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import { Carousel, Slide } from 'vue-carousel';
+
+new Vue({
+	el: "#reviews-component",
+	template: "#reviews-widget",
+	components: {
+		Carousel,
+		Slide
+	}
+
+});
