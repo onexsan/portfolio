11d49cf60df4f4094211f209a6e6ec7c797df635
Victoria Filileeva <onexsan@gmail.com>
2020-01-28 19:40:00 +0300

    added a disabled button mode

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -3,15 +3,58 @@ import { Carousel, Slide } from 'vue-carousel';
 
 const ReviewsCarousel = {
 	template: "#reviews-widget",
+	data() {
+		return {
+			reviews: [],
+			slidesPerPage: 2
+		}
+	},
 	components: {
 		Carousel,
 		Slide
+	},
+	methods: {
+		pageChange(number) {
+			EventBus.$emit('activePage', number);
+		},
+		pages() {
+			if (this.slidesPerPage > 1) {
+				return this.reviews.length % 2 === 1
+					? (this.reviews.length - 1) / 2
+					: this.reviews.length / 2 - 1;
+			}
+
+			return this.reviews.length - 1;
+		},
+		calcSlidesPerPage(self) {
+			function calc() {
+				self.slidesPerPage = (window.innerWidth <= 700) ? 1 : 2;
+			}
+
+			calc();
+
+			window.addEventListener('resize', calc);
+		},
+		mounted() {
+			this.calcSlidesPerPage(this);
+		},
+		watch: {
+			slidesPerPage() {
+				EventBus.$emit('pages', this.pages());
+			}
+		}
+
+
 	}
 
 };
 
 new Vue({
 	el: '.reviews__container',
+	data: {
+		activePage: 0,
+		pages: 0
+	},
 	components: {
 		ReviewsCarousel
 	},
@@ -22,5 +65,14 @@ new Vue({
 		next() {
 			document.querySelector('.VueCarousel-navigation-next').click()
 		}
+	},
+	mounted() {
+		EventBus.$on('activePage', number => {
+			this.activePage = number;
+		});
+
+		EventBus.$on('pages', number => {
+			this.pages = number;
+		});
 	}
 });
